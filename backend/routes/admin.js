3398c489cc330d0968da549a0ea683578beb553b
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-10-14 12:20:13 +0530

    [BE] chore: added admin/staff create

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,9 +1,32 @@
-const router = require('express').Router();
+const express = require('express');
+const bcrypt = require('bcryptjs');
+//const router = require('express').Router();
 const db = require('../utils/db');
 const authorize = require('../middleware/auth');
+const router = express.Router();
+
+// Create new staff or admin (admin-only)
+router.post('/create-user', authorize(['admin']), async (req, res) => {
+  const { username, email, password, role, phone, address } = req.body;
+
+  if (!['staff', 'admin'].includes(role))
+    return res.status(400).json({ error: 'Invalid role type'});
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const [result] = await db.query(
+      'INSERT INTO User (UserName, Email, PasswordHash, PhoneNumber, User_Address, Role) VALUES (?,?,?,?,?,?)',
+      [username, email, hashedPassword, phone, address, role]
+    );
+
+    res.json({ message: `${role} created successfully`, userId: result.insertId});
+  } catch (err) {
+    res.status(500).json({ error: err.message});
+  }
+});
 
 // Add new product
-router.post('/add-product', authorize(['warehouse_admin','system_admin']), async (req, res) => {
+router.post('/add-product', authorize(['admin','admin']), async (req, res) => {
   const { name, description, price, categoryId, SKU } = req.body;
   try {
     const [result] = await db.query(
@@ -11,7 +34,7 @@ router.post('/add-product', authorize(['warehouse_admin','system_admin']), async
        VALUES (?, ?, ?, ?, ?)`,
       [name, description, price, categoryId, SKU]
     );
-    res.json({ message: 'Product added', productId: result.insertId });
+    res.json({ message: 'Product added succesfully', productId: result.insertId });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
